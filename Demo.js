bf670ea6cc51a6560f4ecc9e0801be064903b48b
Ojaswi2000 <65769340+Ojaswi2000@users.noreply.github.com>
2020-09-24 23:57:47 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+export default class Demo extends Component
+{
+  render()
+  {
+    return(
+      <View>
+        <Text>Welcome to React native tutorials.We hope you will do well here.</Text>
+
+      </View>
+    );
+
+  }
+
+}
